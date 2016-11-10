a2126d4d49997b26b82cc1b70943fd4fee8ef0eb
Punal Chotrani <punalsc@gmail.com>
2016-11-10 14:36:29 +0000

    checkbox update.

--- cabinetpapers/scripts/tabs.js
+++ cabinetpapers/scripts/tabs.js
@@ -1,11 +1,21 @@
 $(document).ready(function(){
     $(this).click(function () {
+        if ($('#all').attr('checked')) {
+            $('#all').attr('disabled', true);
+        }
         if ($("input[name='filter[]']").is(':checked')) {
             $('#all').prop('checked',false);
         }
         if ($("input[name='filter[]']:checked").length == $("input[name='filter[]").length) {
             $('#all').prop('checked',true);
             $("input[name='filter[]']").prop('checked',false);
+        }
+        if ($("input[name='filter[]']:checked").length == 0) {
+            $('#all').prop('checked',true);
+            //$("input[name='filter[]']").prop('checked',false);
+        }
+        if ($('#all').attr('checked')) {
+            $('#all').attr('disabled', false);
         }
     });
 });
