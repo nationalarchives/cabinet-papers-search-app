a98645ea4ab30b2a18c2fa641aea42044630cb04
Punal Chotrani <punalsc@gmail.com>
2016-12-02 14:01:51 +0000

    Updated the javascript.

--- cabinetpapers/scripts/tabs.js
+++ cabinetpapers/scripts/tabs.js
@@ -1,30 +1,3 @@
-// $(document).ready(function(){
-//     $(this).click(function () {
-//         if ($("input[name='filter[]']").is(':checked')) {
-//             $('#all').prop('checked',false);
-//         }
-//         if ($("input[name='filter[]']:checked").length == $("input[name='filter[]").length) {
-//             $('#all').prop('checked',true);
-//             $("input[name='filter[]']").prop('checked',false);
-//         }
-//         if ($("input[name='filter[]']:checked").length == 0) {
-//             $('#all').prop('checked',true);
-//             //$("input[name='filter[]']").prop('checked',false);
-//         }
-//         if ($('#all').attr('checked')) {
-//             $('#all').attr('disabled', false);
-//         }
-//         if ($("input[name='filter[]']:checked").length <= 1) {
-//             $("#all").prop("disabled", false);
-//             //$('#all').prop('checked',false);
-//         }
-//         $('#all').change(function(){
-//             $(this).prop('checked',true);
-//             $("input[name='filter[]']").prop('checked',false);
-//         });
-//     });
-// });
-
 $(document).ready(function($){
     //Show/Hide Tabs
     $(".multitab-widget-content-widget-id").hide();
@@ -65,4 +38,8 @@ $(document).ready(function($){
 
     $( "ul.multitab-widget" ).addClass( "show" );
 
+    //Binding the fields
+    $('#exactwords').bind('keypress keyup blur', function() {
+        $('#websearch').val($(this).val());
+    });
 });
